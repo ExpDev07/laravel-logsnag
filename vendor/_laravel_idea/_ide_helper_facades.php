2433e2e89d603f5102ca7048a5594813a6617889
ExpDev07 <mariusrich2@gmail.com>
2022-03-23 20:55:44 +0100

    remove files

---
+++ vendor/_laravel_idea/_ide_helper_facades.php
@@ -0,0 +1,16 @@
+<?php
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+
+namespace ExpDev07\Logsnag\Facades {
+    
+    /**
+     * @see \ExpDev07\Logsnag\Logsnag::log
+     * @method static void log(string $channel, string $event)
+     */
+    class Logsnag {}
+}
+
+namespace {
+
+}
